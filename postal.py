719ae67bd01b6a2bf2c99062ed70f5129403c202
logan tackett <logantackettwork@gmail.com>
2024-11-09 22:04:59 -0800

    truck sort packages function

--- postal.py
+++ postal.py
@@ -1,3 +1,4 @@
+from util import merge_sort, nearestNeighbor
 import csv
 import datetime
 
@@ -48,5 +49,40 @@ class Truck:
     #first sorts packages by deadline to ensure that packages with earlier deadlines get delivered first, O(nlogn).
     #then sorts each group of packages with the same deadline using nearest neighbor, 
     # such that the start of each group is the closest location to the end of the previous group.
-    def sortPackages(self,hashTable,adjMatrix):
-        return
+    #the destination array stores a list of all destinations such that the index of the destination is the vertex label 
+    # in the adjacency matrix
+    def sortPackages(self,hashTable,adjMatrix,destination_array):
+        #key_f function
+        def id_to_deadline(id):
+            package = hashTable.lookup(id)
+            return package.id
+        
+        #key_f function
+        def id_to_adj_label(id):
+            package = hashTable.lookup(id)
+            label = destination_array.index(package.address)
+            return label
+            
+        
+        #sorts packages first by deadline, earlier to latest
+        merge_sort(self.packages,0,self.package_count-1,id_to_deadline)
+
+        l = 0
+        r = 0
+        #will always be the hub.
+        start = 0
+        while l < self.package_count - 1:
+            #adjusting r pointer such that l and r pointer group items with same deadline
+            if id_to_deadline(self.packages[l]) == id_to_deadline(self.packages[r]):
+                r += 1
+            else:
+                #slice that portion of the packages array
+                sliced = self.packages[l:r]
+                #sort it by nearest neighbor
+                optimal = nearestNeighbor(start,sliced,id_to_adj_label,adjMatrix)
+                #copy the sorted items back into the original array
+                for i in range(len(optimal)):
+                    self.packages[l+i] = optimal[i]
+                #adjust pointers
+                l = r
+                start = id_to_adj_label(self.packages[r-1])
